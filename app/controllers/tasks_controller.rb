a9d1920f0fdd4de607e53fdea80ad6f35ee20861
Ida Schwarzkopf <ida.schwarzkopf@ha-es.de>
2022-02-15 12:57:35 +0100

    Can now add a task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,29 @@
 class TasksController < ApplicationController
+  before_action :find_task, only: [:show]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(task_parameters)
+    redirect_to tasks_path
+  end
+
+  private
+
+  def find_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_parameters
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
