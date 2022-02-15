e4d14eb38fae0e3f11528149a838f9d1815094f4
Ida Schwarzkopf <ida.schwarzkopf@ha-es.de>
2022-02-15 13:49:49 +0100

    except checkboxes

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :find_task, only: [:show]
+  before_action :find_task, only: [:show, :edit, :update, :destroy]
 
   def index
     @tasks = Task.all
@@ -14,6 +14,19 @@ class TasksController < ApplicationController
 
   def create
     @task = Task.create(task_parameters)
+    redirect_to task_path(@task)
+  end
+
+  def edit
+  end
+
+  def update
+    @task.update(task_parameters)
+    redirect_to task_path(@task)
+  end
+
+  def destroy
+    @task.destroy
     redirect_to tasks_path
   end
 
